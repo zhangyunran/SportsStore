c574af8d02bf7b8d79b6171cf5fa56103872c5f1
Zhang Yun Ran <yunranzhang2009@hotmail.com>
2019-09-07 15:37:38 -0400

    1[NEW] Add new asp.net mvc project

---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+<Assembly: AssemblyTitle("Vic.SportsStore.WebApp")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Vic.SportsStore.WebApp")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("2fb98b1e-1631-4b93-87a8-57b187311162")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
