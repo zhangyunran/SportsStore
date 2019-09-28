b186108e78dd639d2316d23eec6c2fdb22f933ed
zhangyunran <yunranzhang2009@hotmail.com>
2019-09-28 17:06:38 -0400

    1[NEW] add checkout page

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
@@ -53,5 +53,10 @@ namespace Vic.SportsStore.WebApp.Controllers
         {
             return PartialView(cart);
         }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
