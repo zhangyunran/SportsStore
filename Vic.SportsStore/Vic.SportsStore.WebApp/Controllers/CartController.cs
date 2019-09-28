bf888b0fc3998182b4b1020c14b92f43f7c7e3f8
zhangyunran <yunranzhang2009@hotmail.com>
2019-09-28 16:45:18 -0400

    1[NEW] add cart summary

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
@@ -48,5 +48,10 @@ namespace Vic.SportsStore.WebApp.Controllers
             }
             return RedirectToAction("Index", new { returnUrl });
         }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
     }
 }
