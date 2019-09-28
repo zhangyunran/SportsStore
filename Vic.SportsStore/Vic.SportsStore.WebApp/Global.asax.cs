35b62a7f2867063a49b4d15307f0c71f852b1c53
zhangyunran <yunranzhang2009@hotmail.com>
2019-09-28 16:13:41 -0400

    1[NEW] add Custom Model Binder

--- Vic.SportsStore/Vic.SportsStore.WebApp/Global.asax.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Vic.SportsStore.Domain.Entities;
+using Vic.SportsStore.WebApp.Infrastructure.Binders;
 
 namespace Vic.SportsStore.WebApp
 {
@@ -14,6 +16,7 @@ namespace Vic.SportsStore.WebApp
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             IocConfig.ConfigIoc();
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
 }
---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Entities;
+namespace Vic.SportsStore.WebApp.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            // get the Cart from the session
+            Cart cart = null;
+            if (controllerContext.HttpContext.Session != null)
+            {
+                cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            }
+            // create the Cart if there wasn't one in the session data
+            if (cart == null)
+            {
+                cart = new Cart();
+                if (controllerContext.HttpContext.Session != null)
+                {
+                    controllerContext.HttpContext.Session[sessionKey] = cart;
+                }
+            }
+            // return the cart
+            return cart;
+        }
+    }
+}
