5c1fc34368c08997fbecce3a58cbbb20ef780078
zhangyunran <yunranzhang2009@hotmail.com>
2019-10-26 16:55:27 -0400

    1[NEW] add database migrations

--- Vic.SportsStore/Vic.SportsStore.Domain/Entities/Product.cs
+++ Vic.SportsStore/Vic.SportsStore.Domain/Entities/Product.cs
@@ -12,10 +12,15 @@ namespace Vic.SportsStore.Domain.Entities
     {
         [HiddenInput(DisplayValue = false)]
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
-        public string Category { get; set; }
+        [Required(ErrorMessage = "Please specify a category")]
+        public string Category { get; set; }
     }
 }
